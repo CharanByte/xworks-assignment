a1b970d55d075812bf5de923edddda59e15df754
CharanByte <charan7812@gmail.com>
2024-08-06 18:44:50 +0530

    initial

---
+++ instance variable/AirCondition.java
@@ -0,0 +1,19 @@
+
+public class AirCondition {
+
+    double price;
+    String brand;
+    String color;
+
+    AirCondition(){
+        System.out.println("constructor");
+    }
+
+    public static void main(String[] args) {
+        AirCondition airCondition=new AirCondition();
+        System.out.println(airCondition.price;);
+        System.out.println(airCondition.brand);
+        System.out.println(airCondition.color);
+        
+    }
+}
