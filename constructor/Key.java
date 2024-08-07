ad40e5c77d1b81841f7d79c5eef840a583e1c6f0
CharanByte <charan7812@gmail.com>
2024-08-07 14:22:13 +0530

    initial

---
+++ constructor/Key.java
@@ -0,0 +1,17 @@
+
+public class Key {
+    double weight;
+    String brand;
+
+    Key(double weight,String brand){
+        this.weight=weight;
+        this.brand=brand;
+
+    }
+
+    public void details(){
+        System.out.println(weight);
+        System.out.println(brand);
+    }
+    
+}
