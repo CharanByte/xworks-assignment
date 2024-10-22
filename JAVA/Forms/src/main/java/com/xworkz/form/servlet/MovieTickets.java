d888ae25bf0d30fb4056d90253c6e41db664aa07
CharanByte <charan7812@gmail.com>
2024-10-22 23:01:56 +0530

    movieTicket validation of data

--- JAVA/Forms/src/main/java/com/xworkz/form/servlet/MovieTickets.java
+++ JAVA/Forms/src/main/java/com/xworkz/form/servlet/MovieTickets.java
@@ -10,12 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.xworkz.form.dto.MovieTicketsDTO;
+import com.xworkz.form.service.MovieTicketsService;
+import com.xworkz.form.service.MovieTicketsServiceImpl;
 
-
-
-@WebServlet(loadOnStartup = 1,urlPatterns = "/movie")
+@WebServlet(loadOnStartup = 1, urlPatterns = "/movie")
 public class MovieTickets extends HttpServlet {
-	
+
 	public MovieTickets() {
 
 		System.out.println("no arg const of MovieTicketServlet");
@@ -24,17 +24,28 @@ public class MovieTickets extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		String name=req.getParameter("name");
-		String totalTickets=req.getParameter("totalTickets");
-		String theaterName=req.getParameter("theaterName");
-		String seatType=req.getParameter("seatType");
-		String donation=req.getParameter("donation");
-		String date=req.getParameter("date");
-		String time=req.getParameter("time");
-		int convertedTotalTickets=Integer.valueOf(totalTickets);
-		double convertedDonation=Double.valueOf(donation);
+		String name = req.getParameter("name");
+		String totalTickets = req.getParameter("totalTickets");
+		String theaterName = req.getParameter("theaterName");
+		String seatType = req.getParameter("seatType");
+		String donation = req.getParameter("donation");
+		String date = req.getParameter("date");
+		String time = req.getParameter("time");
+		int convertedTotalTickets = Integer.valueOf(totalTickets);
+		double convertedDonation = Double.valueOf(donation);
 
-		MovieTicketsDTO movieTicketsDTO=new MovieTicketsDTO(name, convertedTotalTickets, theaterName, seatType, convertedDonation, date, time);
+		MovieTicketsDTO movieTicketsDTO = new MovieTicketsDTO(name, convertedTotalTickets, theaterName, seatType,
+				convertedDonation, date, time);
+
+		MovieTicketsService movieTicketsService = new MovieTicketsServiceImpl();
+		boolean valid =movieTicketsService.validate(movieTicketsDTO);
+		
+		if(valid) 
+			System.out.println("Data is valid");
+		
+		else
+			System.out.println("data is  Invalid");
+
 		resp.setContentType("text/html");
 
 		PrintWriter printWriter = resp.getWriter();
@@ -46,5 +57,5 @@ public class MovieTickets extends HttpServlet {
 		printWriter.println("</body>");
 
 		printWriter.println("</html>");
-			}
+	}
 }
