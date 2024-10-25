5a8a42e5afca529f6dd1f38793df37c80a51823d
CharanByte <charan7812@gmail.com>
2024-10-25 18:50:29 +0530

    added register form servlet files

--- JAVA/Registration form/src/main/java/com/xworkz/register/servlet/RegisterServlet.java
+++ JAVA/Registration form/src/main/java/com/xworkz/register/servlet/RegisterServlet.java
@@ -1,11 +1,30 @@
 package com.xworkz.register.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class RegisterServlet extends HttpServlet{
+@WebServlet(loadOnStartup = 1, urlPatterns = "/register")
+public class RegisterServlet extends HttpServlet {
 
-	
 	public RegisterServlet() {
-		System.out.println();
+		System.out.println("running RegisterServlet const");
+	}
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		String name = arg0.getParameter("name");
+		String email = arg0.getParameter("email");
+		String age = arg0.getParameter("age");
+		String city = arg0.getParameter("city");
+		String state = arg0.getParameter("state");
+		
+		int convertedAge=Integer.valueOf(age);
+		
+		
 	}
 }
