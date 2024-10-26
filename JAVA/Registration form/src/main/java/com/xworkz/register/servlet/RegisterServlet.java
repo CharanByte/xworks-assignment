a51bf06c045cecc4d5813267c969f11dfd3c02a9
CharanByte <charan7812@gmail.com>
2024-10-26 18:27:30 +0530

    mofifed RegisterServlet

--- JAVA/Registration form/src/main/java/com/xworkz/register/servlet/RegisterServlet.java
+++ JAVA/Registration form/src/main/java/com/xworkz/register/servlet/RegisterServlet.java
@@ -1,12 +1,16 @@
 package com.xworkz.register.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.xworkz.register.dto.RegisterDTO;
+import com.xworkz.register.service.RegisterServiceImp;
 
 @WebServlet(loadOnStartup = 1, urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
@@ -22,9 +26,20 @@ public class RegisterServlet extends HttpServlet {
 		String age = arg0.getParameter("age");
 		String city = arg0.getParameter("city");
 		String state = arg0.getParameter("state");
-		
-		int convertedAge=Integer.valueOf(age);
-		
-		
+
+		int convertedAge = Integer.valueOf(age);
+
+		RegisterDTO registerDTO = new RegisterDTO(name, email, convertedAge, city, state);
+
+		RegisterServiceImp registerServiceImp = new RegisterServiceImp();
+		boolean isValid = registerServiceImp.isValidAndSave(registerDTO);
+
+		PrintWriter printWriter = arg1.getWriter();
+
+		if (isValid)
+			printWriter.println("Data is valid ");
+		else
+			printWriter.println("Data is not valid");
+
 	}
 }
